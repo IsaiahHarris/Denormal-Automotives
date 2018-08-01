1eef9cd7b3e76179e0a9e2376f65446c8de921ca
Isaiah Harris <harrisfambam@gmail.com>
2018-07-31 15:17:54 -1000

    add user and database

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,1 +1,13 @@
+DROP USER IF EXISTS normal_user;
+DROP DATABASE IF EXISTS normal_cars;
+
+CREATE USER normal_user;
+
+CREATE DATABASE normal_cars 
+OWNER normal_user;
+
+\c normal_cars;
+\i scripts/denormal_data.sql;
+
+
 
